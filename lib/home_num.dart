d99fb98d408fda177a774115be713690d864a62a
hsy-05 <107b11602@mailst.cjcu.edu.tw>
2022-10-25 03:24:00 +0800

    亂數頁面

---
+++ lib/home_num.dart
@@ -0,0 +1,223 @@
+import "dart:math";
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class RandomNumHomePage extends StatefulWidget {
+  const RandomNumHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<RandomNumHomePage> createState() => _RandomNumHomePageState();
+}
+
+class _RandomNumHomePageState extends State<RandomNumHomePage> {
+  final TextEditingController randomController = TextEditingController();//輸入的字串
+  final TextEditingController strNumController = TextEditingController();//輸入字串的個數
+  final TextEditingController strSecNum1Controller = TextEditingController();//輸入字串的個數
+  final TextEditingController strSecNum2Controller = TextEditingController();//輸入字串的個數
+
+  var strVal = "";
+  var strNum = 0;
+  var lines = <String>[]; //總字串
+  String resultList = "";
+
+  //final _randomText = Random();
+  //String resultText = "";
+  //var resultArray = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false, //防止鍵盤覆蓋內容
+        appBar: AppBar(
+            title: const Center(
+              child: Text(
+                "亂數",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+            ),
+            actions: <Widget>[
+              //清除
+              IconButton(
+                icon: const Icon(Icons.next_plan),
+                onPressed: () {
+                  setState(() {});
+                },
+              ),
+            ]),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 50, 15, 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "從",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //數字輸入框
+                      SizedBox(
+                        height: 50,
+                        width: 100,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            isDense: true, //輸入是否爲密集形式
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.brown, width: 3.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.orange, width: 3.0),
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                          controller: strNumController,
+                          style: const TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const Text(
+                        "到",
+                        style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      //數字輸入框
+                      SizedBox(
+                        height: 50,
+                        width: 100,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            isDense: true, //輸入是否爲密集形式
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.brown, width: 3.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                              BorderSide(color: Colors.orange, width: 3.0),
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                          controller: strNumController,
+                          style: const TextStyle(
+                              fontSize: 26, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+
+                    ]),
+                const Text(
+                  "之間選取",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      isDense: true, //輸入是否爲密集形式
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.brown, width: 3.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.orange, width: 3.0),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    controller: strNumController,
+                    style: const TextStyle(
+                        fontSize: 26, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Text(
+                  "個數字",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                //間格
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                    height: 50,
+                    width: 100,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            strVal = randomController.text;
+                            //randomList();
+                            pickRandomItems(strNum);  //結果
+                            logResult();    //debug
+                            WidgetsBinding.instance?.focusManager.primaryFocus?.unfocus(); //鍵盤退出
+                          });
+                        },
+                        child: const Text("確定"))),
+                //間格
+                const SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "亂數結果 : $resultList",
+                  style: const TextStyle(
+                      fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  //全部的字串
+  void logResult() {
+    print('送出的文字: $lines');
+    print('結果：'+ pickRandomItems(strNum));
+    print('strNum：$strNum');
+    print('檢查strNumController.text是否為 INT：' + strNumController.text is int);
+  }
+
+  void _strNUmText() {
+    if(strNumController.text.isNotEmpty){
+      strNum = int.parse(strNumController.text);
+    }
+    else{
+      showWinnerToast();
+    }
+  }
+
+  //選取字串的次數
+  String pickRandomItems(int count) {
+    lines = strVal.split('\n');
+    final list = List.from(lines);
+    _strNUmText();
+    if(strNum <= list.length ){
+      list.shuffle(); // 隨機排序
+      final allList = list.take(count).toList();
+      resultList = allList.join(', ');
+    }
+    else{
+      showWinnerToast();
+    }
+    return resultList; // 從0 - count 獲取元素
+  }
+  // 提示框
+  void showWinnerToast() {
+    Fluttertoast.showToast(
+        msg: "請輸入正確數字",
+        toastLength: Toast.LENGTH_SHORT,  //顯示時間長短
+        gravity: ToastGravity.CENTER,     //顯示位置
+        timeInSecForIosWeb: 1,            //顯示秒數
+        backgroundColor: const Color.fromRGBO(255, 225, 31, 1),  //背景
+        textColor: Colors.black,
+        fontSize: 16.0);
+  }
+
+}
